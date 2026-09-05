48ece3e3d0c04d125d9e50900bf921c4f6504d8e
Oliveirart-dev <artoliveirasp@gmail.com>
2026-09-05 10:53:40 -0300

    Testando...

---
+++ 01-fundamentos-plataforma-java/src/metodos/MetodosAula.java
@@ -0,0 +1,23 @@
+package metodos;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class MetodosAula {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quer ligar a TV? ");
+
+        boolean ligadaOuDesligada = sc.nextBoolean();
+        if(){
+
+        }
+    }
+
+    public String TV(boolean ligadaOuDesligada, int volume, int canal){
+
+    }
+}
